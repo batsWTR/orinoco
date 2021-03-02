e3ef0fc0b6fac9516fd3d0bffde26ead4c9b6cda
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-03-02 10:26:09 +0100

    continue first page

--- js/camerasList.js
+++ js/camerasList.js
@@ -10,7 +10,49 @@ function getCameras(){
     requete.send(null);
     requete.onload = function(){
         let reponse = requete.response;
-        console.log(reponse);
+        for(let i = 0; i < reponse.length; i++){
+           // let name = reponse[i]['name'];
+            let id = reponse[i]['_id'];
+            //let description = reponse[i]['description'];
+            //let price = reponse[i]['price'];
+            //let image = reponse[i]['imageUrl'];
+            let lenses = reponse[i]['lenses'];
+
+            // create card
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.classList.add('col-12');
+            card.classList.add('col-md-6');
+            card.classList.add('col-lg-4');
+            mainContent.appendChild(card);
+
+            // add image
+            let image = document.createElement('img');
+            image.setAttribute('src', reponse[i]['imageUrl']);
+            card.appendChild(image);
+
+            // add name
+            let name = document.createElement('h2');
+            name.textContent = reponse[i]['name'];
+            card.appendChild(name);
+
+            // add description
+            let description = document.createElement('p');
+            description.textContent = reponse[i]['description'];
+            card.appendChild(description);
+
+            // add price
+            let price = document.createElement('span');
+            let euros = parseInt(reponse[i]['price']);
+            euros = euros / 1000;
+            price.textContent = euros + ' \u20ac';
+            card.appendChild(price);
+
+            // add eventlistener
+            card.addEventListener('click', function(event){
+                console.log(name.textContent);
+            });
+        }
     }
 
 }
@@ -18,4 +60,6 @@ function getCameras(){
 
 
 getCameras();
+
+
 
