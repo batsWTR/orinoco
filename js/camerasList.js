ab7d5f5c6d5962356c64162bc74c091b758e8f8e
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-03-04 11:23:16 +0100

    add event listener for DOM

--- js/camerasList.js
+++ js/camerasList.js
@@ -1,67 +1,72 @@
 
 
-let cameraPath = 'http://localhost:3000/api/cameras';
-let mainContent = document.getElementById('content');
-
-function getCameras(){
-    let requete = new XMLHttpRequest();
-    requete.open('GET', cameraPath);
-    requete.responseType = 'json';
-    requete.send(null);
-    requete.onload = function(){
-        let reponse = requete.response;
-        for(let i = 0; i < reponse.length; i++){
-            let id = reponse[i]['_id'];
-
-            // create card
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.classList.add('col-12');
-            card.classList.add('col-md-4');
-            card.classList.add('col-lg-3');
-            card.classList.add('m-2');
-            mainContent.appendChild(card);
-
-            // add image
-            let image = document.createElement('img');
-            image.setAttribute('src', reponse[i]['imageUrl']);
-            card.appendChild(image);
-
-            // add name
-            let name = document.createElement('h2');
-            name.textContent = reponse[i]['name'];
-            card.appendChild(name);
-
-            // add description
-            let description = document.createElement('p');
-            description.textContent = reponse[i]['description'];
-            card.appendChild(description);
-
-            /* options are not important in index page
-            let lenses = document.createElement('span');
-            lenses.textContent =  reponse[i]['lenses'];
-            card.appendChild(lenses);  */
-
-            // add price
-            let price = document.createElement('span');
-            let euros = parseInt(reponse[i]['price']);
-            euros = euros / 100;
-            price.textContent = euros + ' \u20ac';
-            card.appendChild(price);
-
-            // add eventlistener and launch product page with corresponding id
-            card.addEventListener('click', function(event){
-                console.log(name.textContent);
-                window.open('product.html' + '?id=' + id, '_self');
-            });
+// wait until DOM is loaded
+document.addEventListener('DOMContentLoaded', () =>{
+    const cameraPath = 'http://localhost:3000/api/cameras';
+    const mainContent = document.getElementById('content');
+    
+    function getCameras(){
+        let requete = new XMLHttpRequest();
+        requete.open('GET', cameraPath);
+        requete.responseType = 'json';
+        requete.send(null);
+        requete.onload = function(){
+            let reponse = requete.response;
+            for(let i = 0; i < reponse.length; i++){
+                let id = reponse[i]['_id'];
+    
+                // create card
+                let card = document.createElement('div');
+                card.classList.add('card');
+                card.classList.add('col-12');
+                card.classList.add('col-md-4');
+                card.classList.add('col-lg-3');
+                card.classList.add('m-2');
+                mainContent.appendChild(card);
+    
+                // add image
+                let image = document.createElement('img');
+                image.setAttribute('src', reponse[i]['imageUrl']);
+                card.appendChild(image);
+    
+                // add name
+                let name = document.createElement('h2');
+                name.textContent = reponse[i]['name'];
+                card.appendChild(name);
+    
+                // add description
+                let description = document.createElement('p');
+                description.textContent = reponse[i]['description'];
+                card.appendChild(description);
+    
+                /* options are not important in index page
+                let lenses = document.createElement('span');
+                lenses.textContent =  reponse[i]['lenses'];
+                card.appendChild(lenses);  */
+    
+                // add price
+                let price = document.createElement('span');
+                let euros = parseInt(reponse[i]['price']);
+                euros = euros / 100;
+                price.textContent = euros + ' \u20ac';
+                card.appendChild(price);
+    
+                // add eventlistener and launch product page with corresponding id
+                card.addEventListener('click', ()=>{
+                    console.log(name.textContent);
+                    window.open('product.html' + '?id=' + id, '_self');
+                });
+            }
         }
+    
     }
+    
+    
+    
+    getCameras();
+});
 
-}
 
-
-
-getCameras();
 
 
 
--- js/product.js
+++ js/product.js
@@ -2,86 +2,89 @@
 
 // load page for 1 product
 
-
-const cameraPath = 'http://localhost:3000/api/cameras';
-const mainContent = document.getElementById('content');
-
-// get id from product
-let param = new URLSearchParams(window.location.search);
-const id = param.get('id');
-
-function getProduct(){
-    let requete = new XMLHttpRequest();
-    requete.open('GET', cameraPath + '/' + id);
-    requete.responseType = 'json';
-    requete.send(null);
-    requete.onload = function(){
-        let reponse = requete.response;
-        let id = reponse['_id'];
-
-        // create card
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.classList.add('col-6');
-        card.classList.add('m-auto');
-        mainContent.appendChild(card);
-
-        // add image
-        let image = document.createElement('img');
-        image.setAttribute('src', reponse['imageUrl']);
-        card.appendChild(image);
-
-        // add name
-        let name = document.createElement('h2');
-        name.textContent = reponse['name'];
-        card.appendChild(name);
-
-        // add description
-        let description = document.createElement('p');
-        description.textContent = reponse['description'];
-        card.appendChild(description);
-
-        // add lenses options 
-        let lenses = document.createElement('div');
-
-        let labelLenses = document.createElement('label');
-        labelLenses.setAttribute('for', 'lentille');
-        labelLenses.textContent = "Type d'objectif:  ";
-
-        let selectLenses = document.createElement('select');
-        selectLenses.setAttribute('id', 'lentille');
-
-        for (let i = 0; i < reponse['lenses'].length; i++){
-            let option = document.createElement('option');
-            option.setAttribute('value', reponse['lenses'][i]);
-            option.textContent = reponse['lenses'][i];
-            selectLenses.appendChild(option);
+document.addEventListener('DOMContentLoaded', () =>{
+    const cameraPath = 'http://localhost:3000/api/cameras';
+    const mainContent = document.getElementById('content');
+    
+    // get id from product
+    let param = new URLSearchParams(window.location.search);
+    const id = param.get('id');
+    
+    function getProduct(){
+        let requete = new XMLHttpRequest();
+        requete.open('GET', cameraPath + '/' + id);
+        requete.responseType = 'json';
+        requete.send(null);
+        requete.onload = function(){
+            let reponse = requete.response;
+            let id = reponse['_id'];
+    
+            // create card
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.classList.add('col-6');
+            card.classList.add('m-auto');
+            mainContent.appendChild(card);
+    
+            // add image
+            let image = document.createElement('img');
+            image.setAttribute('src', reponse['imageUrl']);
+            card.appendChild(image);
+    
+            // add name
+            let name = document.createElement('h2');
+            name.textContent = reponse['name'];
+            card.appendChild(name);
+    
+            // add description
+            let description = document.createElement('p');
+            description.textContent = reponse['description'];
+            card.appendChild(description);
+    
+            // add lenses options 
+            let lenses = document.createElement('div');
+    
+            let labelLenses = document.createElement('label');
+            labelLenses.setAttribute('for', 'lentille');
+            labelLenses.textContent = "Type d'objectif:  ";
+    
+            let selectLenses = document.createElement('select');
+            selectLenses.setAttribute('id', 'lentille');
+    
+            for (let i = 0; i < reponse['lenses'].length; i++){
+                let option = document.createElement('option');
+                option.setAttribute('value', reponse['lenses'][i]);
+                option.textContent = reponse['lenses'][i];
+                selectLenses.appendChild(option);
+            }
+    
+            lenses.appendChild(labelLenses);
+            lenses.appendChild(selectLenses);
+         
+    
+            card.appendChild(lenses);  
+    
+            // add price
+            let price = document.createElement('span');
+            let euros = parseInt(reponse['price']);
+            euros = euros / 100;
+            price.textContent = 'Prix : ' + euros + ' \u20ac';
+            card.appendChild(price);
+    
+            // add button
+            let button = document.createElement('button');
+            button.textContent = 'Ajouter au panier';
+            button.classList.add('btn');
+            button.classList.add('btn-primary');
+            button.classList.add('col-4');
+            button.classList.add('m-auto');
+            card.appendChild(button);
         }
-
-        lenses.appendChild(labelLenses);
-        lenses.appendChild(selectLenses);
-     
-
-        card.appendChild(lenses);  
-
-        // add price
-        let price = document.createElement('span');
-        let euros = parseInt(reponse['price']);
-        euros = euros / 100;
-        price.textContent = 'Prix : ' + euros + ' \u20ac';
-        card.appendChild(price);
-
-        // add button
-        let button = document.createElement('button');
-        button.textContent = 'Ajouter au panier';
-        button.classList.add('btn');
-        button.classList.add('btn-primary');
-        button.classList.add('col-4');
-        button.classList.add('m-auto');
-        card.appendChild(button);
+    
     }
+    
+    getProduct();
+});
 
-}
 
-getProduct();
 
